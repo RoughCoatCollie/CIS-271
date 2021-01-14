d95a5db9eb55b9e3359bef672c302b3f8cfd4ec2
Cuyamaca <cuyamaca@Newtons-MacBook-Pro.local>
2021-01-13 16:57:01 -0800

    Scripts to correct CIS-271 lab issues

---
+++ FTPBruteForce.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Executing FTP Brute Force Login: 15 attempts, 500ms interval"
+for i in {1..15}
+do
+  curl -s -m 0.5 ftp://admin:badpass$i@192.168.50.10
+  echo "FTP Brute Force Attempt: $i of 15"
+done
+echo "Completed. Please allow 1 minute for generaton of NGFW threat logs."
---
+++ ResetLDAPPassword.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pass=Pal0Alt0
+lab9_ldap_user=lab-user-id
+lab10_ldap_user=lab-user
+
+echo "Resetting Password For LDAP User: $lab9_ldap_user"
+(echo "$pass"; echo "$pass") | smbpasswd -s -U $lab9_ldap_user
+
+echo "Resetting Password For LDAP User: $lab10_ldap_user"
+(echo "$pass"; echo "$pass") | smbpasswd -s -U $lab10_ldap_user
